619d3bb8051c4f94a2d0b7b96d06dd72e3ff4c0d
Rob8624 <robert_melen@yahoo.co.uk>
2022-12-07 12:25:25 +0000

    shortened form field

--- word_game/words/urls.py
+++ word_game/words/urls.py
@@ -3,6 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.homeView, name='home'),#this is the home url
-    path('search', views.searchView, name='search'),#this is the search url
+    path('', views.home_view, name='home'),#this is the home url
+    path('search', views.search_view, name='search'),#this is the search url
 ]
